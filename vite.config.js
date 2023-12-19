6de1be69674b3649e38bfb10e95ebbb4a5b8b4d8
nazmulhasanDEV <nazmulhasan747406@gmail.com>
2023-12-20 05:03:10 +0600

    updated readme file

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // error occured "Loading module from “http://localhost:5173/node_modules/.vite/deps/html-react-parser.js?v=0922589b” was blocked because of a disallowed MIME type (“”)"
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
   resolve: {
     alias: {
-      "@": "/src",
-    },
-  },
+      "@": "/src"
+    }
+  }
 });
