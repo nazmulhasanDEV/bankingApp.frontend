9a982b0bf76ad646bfa248a841e1cac4bc808f62
Zitan Salehin <zitan-74@dipti.com.bd>
2023-12-20 02:45:10 +0600

    warning page

--- src/views/pages/homePage/index.jsx
+++ src/views/pages/homePage/index.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
 import logo from "@/assets/common-assets/images/boq_logo_prev_ui.png";
-import { ArrowRight, ChevronDown, Search, X } from "react-feather";
-import Submenus from "./Submenus";
+import {
+  ArrowRight,
+  Award,
+  ChevronDown,
+  ChevronRight,
+  Search,
+  X,
+} from "react-feather";
 import FeatureCards from "./FeatureCards";
 import Footer from "./Footer";
+import { NavLink } from "react-router-dom";
+import { TabNew, TabsNew } from "../../../components/ui/appTab/AppTabNew";
 
 const HomePage = () => {
   const [selectedItem, setSelectedItem] = useState(null);
@@ -13,8 +21,61 @@ const HomePage = () => {
     setSelectedItem(index);
   };
 
-  const listItemsFirst = ["Personal", "Business"];
-  const listItemsSecond = ["Blog", "About Us", "Contact Us", "Help & Support"];
+  const listItemsFirst = [
+    "Personal",
+    "Banking",
+    "Home Loans",
+    "Business",
+    "Help & Support",
+  ];
+
+  const BankAccount = () => (
+    <div className="flex gap-8 justify-between">
+      <div className="flex flex-col gap-8 flex-wrap w-1/6">
+        <p className="text-3xl font-bold">Everyday Banking With BOQ</p>
+        <ul className="font thin gap-2 flex flex-col">
+          <li className="hover:text-yellow-500 cursor-pointer">
+            Mobile Banking
+          </li>
+          <li className="hover:text-yellow-500 cursor-pointer">Home Loan</li>
+          <li className="hover:text-yellow-500 cursor-pointer">Credit Card</li>
+          <li className="hover:text-yellow-500 cursor-pointer">Insurance</li>
+          <li className="hover:text-yellow-500 cursor-pointer">
+            Personal Loan
+          </li>
+        </ul>
+      </div>
+      <div className="w-3/6 bg-blue-500 p-5">
+        <p className="text-white py-2 text-2xl">
+          WATCH YOUR MONEY GROW WITH OUR HIGH INTEREST TERM DEPOSIT ACCOUNT
+        </p>
+        <p className="text-white text-lg">Term Deposits</p>
+        <p className="text-white text-2xl py-3">5.10% P.A.</p>
+        <p className="text-white pb-5">
+          12 month fixed term deposit from $5,000
+        </p>
+        <p className="text-white font-thin flex items-center cursor-pointer">
+          Learn more <ArrowRight className="w-4 h-4" />
+        </p>
+      </div>
+      <div className="w-2/6 bg-gray-200 p-5">
+        <span>
+          <Award className="text-yellow-600" />
+        </span>
+        <p className="text-gray-600 py-2 text-2xl">
+          Important changes for BOQ Business Banking customers.
+        </p>
+        <p className="text-gray-600">
+          We are making some changes to the BOQ Business Facility General
+          Conditions.
+        </p>
+        <p className="text-gray-600 py-3">
+          Home Loan Awards & Customer Service Award
+        </p>
+      </div>
+    </div>
+  );
+
   return (
     <div className="background-home relative">
       <header className="header-home">
@@ -26,20 +87,27 @@ const HomePage = () => {
             {listItemsFirst.map((item, index) => (
               <li
                 key={index}
-                className={index === selectedItem ? "clicked" : ""}
+                className={`${
+                  index === selectedItem ? "clicked" : ""
+                } flex items-center pl-2 mx-2`}
                 onClick={() => handleItemClick(index)}
               >
                 {item}
+                <span className="pl-2">
+                  <ChevronDown className="w-4 h-5" />
+                </span>
               </li>
             ))}
           </ul>
-          <div className="flex gap-10">
-            {listItemsSecond.map((item, index) => (
-              <span key={index}>{item}</span>
-            ))}
+          <div className="flex items-center gap-10">
             <button onClick={() => setShowSearchField(!showSearchField)}>
               <Search className="text-blue-400 w-6 h-6" />
             </button>
+            <NavLink to="/warning">
+              <button className="p-2 block items-center gap-2 text-white bg-blue-600 px-6 cursor-pointer">
+                LogOn
+              </button>
+            </NavLink>
             {showSearchField && (
               <div
                 className={` ${
@@ -71,121 +139,164 @@ const HomePage = () => {
           </div>
         </div>
       </header>
-
-      <div
-        className="text-black homepage-body"
-      >
-        <Submenus />
-      </div>
       <div>
         <img
-          src="./src/assets/common-assets/images/hero-img.jpeg"
+          src="./src/assets/common-assets/images/hero-img2.jpg"
           alt=""
           className="w-[100vw]"
         />
-
       </div>
-      <div className="absolute top-44 left-44" style={{zIndex:'2'}}>
-          <div className="flex flex-col gap-4">
-            <p className="text-4xl font-bold">Mobile Banking</p>
-            <p className="text-yellow-600 text-xl font-bold">Banking in your palms</p>
-            <div className="flex gap-4">
-              <button className="p-4 bg-blue-500 flex gap-2">Contact Your Local Branch <ArrowRight className="w-6 h-6 text-yellow-400"/></button>
-              <button className="p-4 bg-white text-blue-500 flex gap-2">Call 573284 893 <ArrowRight className="w-6 h-6 "/></button>
-            </div>
+      <div className="absolute top-44 left-44" style={{ zIndex: "2" }}>
+        <div className="flex flex-col gap-6">
+          <p className="text-4xl font-bold">myBOQ</p>
+          <p className="text-yellow-300 text-xl font-medium">
+            Download the featured pack myBOQ App and open a new account in
+            minute
+          </p>
+          <div>
+            <button className="px-4 py-3 bg-blue-600 flex items-center gap-2">
+              <p>Learn More</p>
+              <ArrowRight className="w-4 h-4 text-yellow-400" />
+            </button>
           </div>
         </div>
-        
-        <div className="text-black w-[80%] m-auto">
-          <div className="flex items-center gap-8 justify-center mt-5">
+      </div>
 
-          <p className="border-b-4 border-transparent hover:border-yellow-500 w-fit">Feature</p>
-          <p className="border-b-4 border-transparent hover:border-yellow-500 w-fit">Apply</p>
-          <p className="border-b-4 border-transparent hover:border-yellow-500 w-fit">Important Information</p>
-          <p className="border-b-4 border-transparent hover:border-yellow-500 w-fit">Help</p>
-          <button className="flex items-center gap-2 bg-yellow-500 p-4">
-            Log On To Internet Banking <ArrowRight className="w-5 h-5"/>
-          </button>
+      <div className="text-gray-600 w-[80%] m-auto">
+        <TabsNew>
+          <TabNew component={<BankAccount />} active>
+            Bank Accounts &nbsp;|
+          </TabNew>
+          <TabNew component="in progress home loan section">
+            Home Loan &nbsp;|
+          </TabNew>
+          <TabNew component="in progress credit card section">
+            Credit Cards &nbsp;|
+          </TabNew>
+          <TabNew component="in progress insurance section">
+            Insurance &nbsp;|
+          </TabNew>
+        </TabsNew>
+      </div>
+      <div className="border-b-[1px] border-gray-300 border-opacity-40 w-[80%] my-5 m-auto"></div>
+      <div className="text-gray-600 w-[80%] m-auto">
+        <div className="flex gap-32 justify-between">
+          <div className="flex flex-col gap-8 flex-wrap">
+            <p className="text-3xl font-bold">Our Latest Awards</p>
+            <span className="flex gap-5 items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjO7rCglG7srkC7IvXg0GAGRJUV0vS_4zmwMbmqKHN8fbxUkmXYP37I-lYVgHduHrohLo&usqp=CAU"
+                className="w-32"
+                alt=""
+                srcset=""
+              />
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQftazGeqmCY8c4zjFiKsW-JqNZiexrTQWt4THZzZvBwBal8RGxYu3mZyi9jwN9HIGKckM&usqp=CAU"
+                className="w-24 h-24"
+                alt=""
+                srcset=""
+              />
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjO7rCglG7srkC7IvXg0GAGRJUV0vS_4zmwMbmqKHN8fbxUkmXYP37I-lYVgHduHrohLo&usqp=CAU"
+                className="w-32"
+                alt=""
+                srcset=""
+              />
+            </span>
           </div>
         </div>
-        <div className="border-b-[1px] border-gray-300 border-opacity-40 w-[80%] my-5 m-auto"></div>
-        <div className="text-black w-[80%] m-auto">
-          <div className="flex gap-32 justify-between">
-          <div className="flex flex-col gap-8 flex-wrap w-1/5">
-            <p className="text-4xl font-bold">BOQ Mobile features</p>
-            <p className="font-thin">BOQ Mobile puts you in charge of how you view BOQ on your mobile device.</p>
-          </div>
-          <div className="w-4/5">
-            <FeatureCards/>
-          </div>
-          </div>
-        </div>
-        <div className="border-b-[1px] border-gray-300 border-opacity-40 w-[80%] my-5 m-auto"></div>
-        <div className="text-black w-[80%] m-auto">
-          <div className="flex gap-32 justify-between">
+      </div>
+      <div className="border-b-[1px] border-gray-300 border-opacity-40 w-[80%] my-5 m-auto"></div>
+      <div className="text-black w-[80%] m-auto">
+        <div className="flex gap-32 justify-between">
           <div className="flex flex-col gap-8 flex-wrap w-1/5">
             <p className="text-4xl font-bold">How do I get BOQ Mobile?</p>
           </div>
           <div className="w-4/5 flex flex-col gap-5">
             <p className="text-2xl font-bold">BOQ Internet Banking customers</p>
-            <p className="font-thin">If you have registered for Internet Banking, you can download BOQ Mobile using the below links:</p>
+            <p className="font-thin">
+              If you have registered for Internet Banking, you can download BOQ
+              Mobile using the below links:
+            </p>
             <div className=" bg-gray-900">
-    <div className="max-w-2xl mx-auto text-white py-10">
-        <div className="text-center">
-            <div className="flex justify-center my-10">
-                <div className="flex items-center border rounded-lg px-4 py-2 w-52 mx-2">
-                    <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-8"/>
-                    <div className="text-left ml-3">
-                        <p className='text-xs text-gray-200'>Download on </p>
-                        <p className="text-sm md:text-base"> Google Play Store </p>
+              <div className="max-w-2xl mx-auto text-white py-10">
+                <div className="text-center">
+                  <div className="flex justify-center my-10">
+                    <div className="flex items-center border rounded-lg px-4 py-2 w-52 mx-2">
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
+                        className="w-7 md:w-8"
+                      />
+                      <div className="text-left ml-3">
+                        <p className="text-xs text-gray-200">Download on </p>
+                        <p className="text-sm md:text-base">
+                          {" "}
+                          Google Play Store{" "}
+                        </p>
+                      </div>
                     </div>
-                </div>
-                <div className="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
-                    <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" className="w-7 md:w-8"/>
-                    <div className="text-left ml-3">
-                        <p className='text-xs text-gray-200'>Download on </p>
+                    <div className="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
+                        className="w-7 md:w-8"
+                      />
+                      <div className="text-left ml-3">
+                        <p className="text-xs text-gray-200">Download on </p>
                         <p className="text-sm md:text-base"> Apple Store </p>
+                      </div>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
-        
-    </div>
-</div>
-    <div className="flex flex-col gap-4">
-          <p className="text-2xl">BOQ customers without Internet Banking</p>
-          <ul className="list-disc">
-            <li>Register for Internet Banking.</li>
-            <li>Download the BOQ Mobile app using the links above.</li>
-          </ul>
-        </div>
-          </div>
+            <div className="flex flex-col gap-4">
+              <p className="text-2xl">BOQ customers without Internet Banking</p>
+              <ul className="list-disc">
+                <li>Register for Internet Banking.</li>
+                <li>Download the BOQ Mobile app using the links above.</li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="border-b-[1px] border-gray-300 border-opacity-40 w-[80%] my-5 m-auto"></div>
-        <div className="text-black w-[80%] m-auto">
-          <div className="flex gap-32 justify-between">
+      </div>
+      <div className="border-b-[1px] border-gray-300 border-opacity-40 w-[80%] my-5 m-auto"></div>
+      <div className="text-black w-[80%] m-auto">
+        <div className="flex gap-32 justify-between">
           <div className="flex flex-col gap-8 flex-wrap w-1/5">
             <p className="text-4xl font-bold">Important information</p>
           </div>
           <div className="w-4/5">
-            <p className="font-thin">Before you use Mobile Banking, you must view and accept the Electronic Banking Terms and Conditions. You must Log On to Internet Banking to accept the Terms and Conditions before you use Mobile Banking. You will be shown the Electronic Banking Terms and Conditions the first time you Log On to Internet Banking. You can also view them on the Terms and Conditions page at any time.</p>
-          </div>
+            <p className="font-thin">
+              Before you use Mobile Banking, you must view and accept the
+              Electronic Banking Terms and Conditions. You must Log On to
+              Internet Banking to accept the Terms and Conditions before you use
+              Mobile Banking. You will be shown the Electronic Banking Terms and
+              Conditions the first time you Log On to Internet Banking. You can
+              also view them on the Terms and Conditions page at any time.
+            </p>
           </div>
         </div>
-        <div className="border-b-[1px] border-gray-300 border-opacity-40 w-[80%] my-5 m-auto"></div>
-        <div className="text-black w-[80%] flex m-auto">
-          <div className="flex gap-10">
+      </div>
+      <div className="border-b-[1px] border-gray-300 border-opacity-40 w-[80%] my-5 m-auto"></div>
+      <div className="text-black w-[80%] flex m-auto">
+        <div className="flex gap-10">
           <div className="flex flex-col flex-wrap">
-            <img src="https://www.boq.com.au/content/dam/boq/images/content-tile/young-man-laptop-content.jpg" alt="" srcset="" />
+            <img
+              src="https://www.boq.com.au/content/dam/boq/images/content-tile/young-man-laptop-content.jpg"
+              alt=""
+              srcset=""
+            />
           </div>
           <div className="w-4/5 flex flex-col gap-5">
             <p className="font-thin text-xl">Need help with Mobile Banking?</p>
-            <button className="p-4 bg-blue-500 text-white flex gap-2 w-fit">View online banking help <ArrowRight className="w-6 h-6 text-yellow-400"/></button>
-          </div>
+            <button className="p-4 bg-blue-500 text-white flex gap-2 w-fit">
+              View online banking help{" "}
+              <ArrowRight className="w-6 h-6 text-yellow-400" />
+            </button>
           </div>
         </div>
-        <Footer/>
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/views/pages/warningPage/index.jsx
+++ src/views/pages/warningPage/index.jsx
@@ -4,31 +4,41 @@ import { NavLink } from "react-router-dom";
 
 const WarningPage = () => {
   return (
-    <div className="bg-white warning-page pl-20 pt-10">
-      <div className="">
-        <h2>Remote Access Scam</h2>
-        <ul className="list-disc">
-          <li>
-            Receiving a call from someone advising they have detected internet
-            or performance issues with your computer or modem.
-          </li>
-          <li>
-            The caller asking you to buy a piece of software or a service to
-            ‘fix’ your computer.
-          </li>
-          <li>
-            Caller asking you for personal details and/or banking or credit card
-            details.
-          </li>
-          <li>They may then ask for some or all of the following:</li>
-          <ul className="list-disc">
-            <li>Remote access to your computer.</li>
-            <li>You to log into your internet banking.</li>
-            <li>A need to refund your account.</li>
-            <li>You to return the refund that was 'overpaid'.</li>
+    <div className="bg-white warning-page p-8 overflow-hidden overflow-y-scroll">
+      <div className="max-w-2xl mx-auto">
+        <p className="text-gray-800 font-semibold text-3xl mb-6">
+          Latest Scams And Warnings
+        </p>
+
+        <div className="mb-8">
+          <p className="text-gray-800 font-semibold text-xl mb-2">
+            Remote Access Scam
+          </p>
+          <ul className=" pl-5 mb-4">
+            <li>
+              Receiving a call from someone advising they have detected internet
+              or performance issues with your computer or modem.
+            </li>
+            <li>
+              The caller asking you to buy a piece of software or a service to
+              ‘fix’ your computer.
+            </li>
+            <li>
+              Caller asking you for personal details and/or banking or credit
+              card details.
+            </li>
+            <li>They may then ask for some or all of the following:</li>
+            <ul className=" pl-5">
+              <li>Remote access to your computer.</li>
+              <li>You to log into your internet banking.</li>
+              <li>A need to refund your account.</li>
+              <li>You to return the refund that was 'overpaid'.</li>
+            </ul>
           </ul>
-          <li>How to protect yourself from a remote access scam:</li>
-          <ul className="list-disc">
+          <p className="font-semibold mb-2">
+            How to protect yourself from a remote access scam:
+          </p>
+          <ul className=" pl-5 mb-4">
             <li>
               If you receive a phone call out of the blue about your computer or
               internet service and remote access is requested – hang up – even
@@ -58,36 +68,38 @@ const WarningPage = () => {
               address and phone numbers.
             </li>
           </ul>
-        </ul>
+        </div>
 
-        <h2>Phishing</h2>
-        <ul className="list-disc">
-          <li>
-            A phishing scam is a fraudulent attempt to obtain sensitive
-            information or data from you such as bank account numbers,
-            passwords, and credit card numbers.
-          </li>
-          <li>
-            You may be contacted by email, social media, phone call, or text
-            message.
-          </li>
-          <li>Warning signs:</li>
-          <ul className="list-disc">
+        <div className="mb-8">
+          <p className="text-gray-800 font-semibold text-xl mb-2">Phishing</p>
+          <ul className=" pl-5 mb-4">
             <li>
-              The message is sent from a public email domain. Legitimate
-              organisations will typically not send you emails from a free
-              webmail service.
+              A phishing scam is a fraudulent attempt to obtain sensitive
+              information or data from you such as bank account numbers,
+              passwords, and credit card numbers.
             </li>
-            <li>The domain name is misspelt.</li>
-            <li>The email is poorly written.</li>
-            <li>It includes suspicious attachments or links.</li>
-            <li>The message creates a sense of urgency.</li>
             <li>
-              The messages ask you to log into your Internet Banking account.
+              You may be contacted by email, social media, phone call, or text
+              message.
             </li>
+            <li>Warning signs:</li>
+            <ul className=" pl-5">
+              <li>
+                The message is sent from a public email domain. Legitimate
+                organisations will typically not send you emails from a free
+                webmail service.
+              </li>
+              <li>The domain name is misspelt.</li>
+              <li>The email is poorly written.</li>
+              <li>It includes suspicious attachments or links.</li>
+              <li>The message creates a sense of urgency.</li>
+              <li>
+                The messages ask you to log into your Internet Banking account.
+              </li>
+            </ul>
           </ul>
-          <li>How to protect yourself:</li>
-          <ul className="list-disc">
+          <p className="font-semibold mb-2">How to protect yourself:</p>
+          <ul className=" pl-5 mb-4">
             <li>Be cautious and validate all communications you receive.</li>
             <li>
               Do not click on any links listed in the correspondence and do not
@@ -102,7 +114,10 @@ const WarningPage = () => {
             <li>
               If you receive a link posing to be from BOQ, report it to us
               immediately via{" "}
-              <a href="mailto:financialcrimes@boq.com.au">
+              <a
+                href="mailto:financialcrimes@boq.com.au"
+                className="text-blue-500"
+              >
                 financialcrimes@boq.com.au
               </a>
               .
@@ -111,31 +126,34 @@ const WarningPage = () => {
               Delete the message, ensuring to take a screenshot if you need to
               report the URL to BOQ.
             </li>
-            <li>
-              Always access Internet Banking via{" "}
-              <a href="https://www.boq.com.au">www.boq.com.au</a> or the BOQ
-              mobile app.
-            </li>
-          </ul>
-        </ul>
-        <div className="flex gap-5 items-center absolute bottom-20 right-20">
-          <div className="flex items-center space-x-3 py-6">
-            <input
-              type="checkbox"
-              className="border-gray-300 rounded h-5 w-5"
-            />
+            <div className="flex justify-between w-[60rem]">
+              <li>
+                Always access Internet Banking via{" "}
+                <a href="https://www.boq.com.au" className="text-blue-500">
+                  www.boq.com.au
+                </a>{" "}
+                or the BOQ mobile app.
+              </li>
 
-            <div>
-              <p className="text-xs text-gray-500 leading-4">
-                Don't show this message again
-              </p>
+              <div className="flex gap-5 items-center">
+                <div className="flex items-center space-x-3 mb-4">
+                  <input
+                    type="checkbox"
+                    className="border-gray-300 rounded h-5 w-5"
+                  />
+                  <p className="text-xs text-gray-500 leading-4">
+                    Don't show this message again
+                  </p>
+                </div>
+
+                <NavLink to="/login">
+                  <button className="bg-blue-500 text-white px-4 py-2 h-10 flex items-center rounded-md mb-4">
+                    Continue <ArrowRight className="w-5 h-5 ml-2" />
+                  </button>
+                </NavLink>
+              </div>
             </div>
-          </div>
-          <NavLink to="/login">
-            <button className="bg-blue-500 text-white px-4 py-2 h-10 flex items-center rounded-md ">
-              Continue <ArrowRight className="w-5 h-5" />
-            </button>
-          </NavLink>
+          </ul>
         </div>
       </div>
     </div>
